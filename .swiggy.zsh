1aede7bb4aa8f7aef488151fff8187547f2c60e1
Bhavi Dhingra <bhavi.dhingra1@MacBook-Pro.local>
2022-11-23 01:59:44 +0530

    refactor: .swiggy.zsh

--- .swiggy.zsh
+++ .swiggy.zsh
@@ -1,8 +1,4 @@
-# Directories
-alias custom-coast-cli='cd $HOME/SF_Repos/custom-coast-cli'
-
-# Commands
 alias gradlebuild='./gradlew spotlessApply; ./gradlew clean build jettyRun --stacktrace'
 
-export PATH=$PATH:$HOME/SF_Repos/custom-coast-cli
-export GOPRIVATE="bitbucket.org/swigy/*,github.com/swiggy-private/*"
+export PATH=$PATH:$HOME/Github/custom-coast-cli
+export GOPRIVATE="github.com/swiggy-private/*"
