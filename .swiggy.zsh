f83670b907c27a3b837b49528f8e92bd70cd4106
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2022-11-23 01:41:08 +0530

    feat: add .swiggy.zsh

---
+++ .swiggy.zsh
@@ -0,0 +1,8 @@
+# Directories
+alias custom-coast-cli='cd $HOME/SF_Repos/custom-coast-cli'
+
+# Commands
+alias gradlebuild='./gradlew spotlessApply; ./gradlew clean build jettyRun --stacktrace'
+
+export PATH=$PATH:$HOME/SF_Repos/custom-coast-cli
+export GOPRIVATE="bitbucket.org/swigy/*,github.com/swiggy-private/*"
